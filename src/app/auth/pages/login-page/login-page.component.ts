1d643360fac68d1fe23da98bbfc2a4e839261987
Marcelino Ponce Fajardo <mponce@iodigital.es>
2024-05-24 10:47:32 +0200

    Initial commit

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styles: [
+  ]
+})
+export class LoginPageComponent {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onLogin(): void {
+
+    this.authService.login('test@gmail.com', '123456')
+      .subscribe(user => {
+        this.router.navigate(['/']);
+      });
+  }
+}
---
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+  styles: [
+  ]
+})
+export class LayoutPageComponent {
+
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list'},
+    { label: 'Añadir', icon: 'add', url: './new-hero'},
+    { label: 'Buscar', icon: 'search', url: './search'},
+  ]
+
+  constructor(private authService: AuthService) {}
+
+  onLogout() {
+    this.authService
+  }
+}
